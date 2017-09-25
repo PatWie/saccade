4157436875610c4bbfe78aa78e020139696c80a7
PatWie <mail@patwie.com>
2017-09-25 02:46:10 +0200

    project link

--- GUI/about_window.cpp
+++ GUI/about_window.cpp
@@ -2,13 +2,15 @@
 #include "ui_about_window.h"
 
 GUI::AboutWindow::AboutWindow(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::AboutWindow)
-{
-    ui->setupUi(this);
+  QDialog(parent),
+  ui(new Ui::AboutWindow) {
+  ui->setupUi(this);
+
+  QPixmap icon_png(":Icon/256x256/eagleeye.png");
+  icon_png = icon_png.scaled(QSize(128, 128), Qt::IgnoreAspectRatio);
+  ui->lbl_icon->setPixmap(icon_png);
 }
 
-GUI::AboutWindow::~AboutWindow()
-{
-    delete ui;
+GUI::AboutWindow::~AboutWindow() {
+  delete ui;
 }
